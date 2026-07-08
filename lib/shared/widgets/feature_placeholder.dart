2f7504fafbbb176fd893dbfdcda95e6ed89f0d04
samihchaa-wq <samihchaa@hotmail.com>
2026-07-09 01:08:11 +0200

    feat(flutter): add shared placeholder

---
+++ lib/shared/widgets/feature_placeholder.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FeaturePlaceholder extends StatelessWidget {
+  const FeaturePlaceholder({required this.title, required this.icon, super.key});
+
+  final String title;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: Text(title)),
+        body: Center(child: Text(title)),
+      );
+}
