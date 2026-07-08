2e66602a157a72d4ffcf670c436b790ef6601a21
samihchaa-wq <samihchaa@hotmail.com>
2026-07-09 01:08:20 +0200

    feat(flutter): add matches feature

---
+++ lib/features/matches/presentation/matches_page.dart
@@ -0,0 +1,12 @@
+import 'package:as_grinta/shared/widgets/feature_placeholder.dart';
+import 'package:flutter/material.dart';
+
+class MatchesPage extends StatelessWidget {
+  const MatchesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => const FeaturePlaceholder(
+        title: 'Matchs',
+        icon: Icons.sports_soccer,
+      );
+}
