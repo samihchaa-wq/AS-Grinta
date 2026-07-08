45113587a066edd0e81f79eb5447955d39e821ca
samihchaa-wq <samihchaa@hotmail.com>
2026-07-09 01:08:29 +0200

    feat(flutter): add statistics feature

---
+++ lib/features/statistics/presentation/statistics_page.dart
@@ -0,0 +1,12 @@
+import 'package:as_grinta/shared/widgets/feature_placeholder.dart';
+import 'package:flutter/material.dart';
+
+class StatisticsPage extends StatelessWidget {
+  const StatisticsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => const FeaturePlaceholder(
+        title: 'Statistiques',
+        icon: Icons.bar_chart,
+      );
+}
