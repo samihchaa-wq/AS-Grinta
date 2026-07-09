bf627f62fb7da155fc6f283cc76a16012d931bba
samihchaa-wq <samihchaa@hotmail.com>
2026-07-09 05:42:11 +0200

    test: compile all critical V1 screens

---
+++ test/smoke/critical_screens_compile_test.dart
@@ -0,0 +1,29 @@
+import 'package:as_grinta/features/admin/presentation/admin_page.dart';
+import 'package:as_grinta/features/live/presentation/live_gameplay_page.dart';
+import 'package:as_grinta/features/live/presentation/live_page.dart';
+import 'package:as_grinta/features/matches/presentation/match_correction_page.dart';
+import 'package:as_grinta/features/matches/presentation/match_form_page.dart';
+import 'package:as_grinta/features/predictions/presentation/leaderboard_page.dart';
+import 'package:as_grinta/features/predictions/presentation/season_predictions_page.dart';
+import 'package:as_grinta/features/profile/presentation/profile_page.dart';
+import 'package:as_grinta/features/statistics/presentation/statistics_page.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('critical V1 screens compile', () {
+    const screens = <Widget>[
+      AdminPage(),
+      LivePage(matchId: 'compile-test'),
+      LiveGameplayPage(matchId: 'compile-test'),
+      MatchCorrectionPage(matchId: 'compile-test'),
+      MatchFormPage(),
+      LeaderboardPage(),
+      SeasonPredictionsPage(),
+      ProfilePage(),
+      StatisticsPage(),
+    ];
+
+    expect(screens.length, 9);
+  });
+}
