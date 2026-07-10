11212c663c2dc45212dd696d64c328b22c01098d
samihchaa-wq <samihchaa@hotmail.com>
2026-07-10 11:04:06 +0200

    Supprimer le provider de statut live

--- lib/features/coach/presentation/coach_match_status_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:as_grinta/features/coach/data/coach_live_repository.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final coachMatchStatusProvider =
-    StreamProvider.autoDispose.family<String, String>((ref, matchId) {
-  return ref.watch(coachLiveRepositoryProvider).watchMatchStatus(matchId);
-});
