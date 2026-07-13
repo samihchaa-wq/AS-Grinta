ff756c5c1805384f7a70e6b2cb2036989733f52b
samihchaa-wq <samihchaa@hotmail.com>
2026-07-14 01:28:04 +0200

    chore: apply Dart formatting

--- lib/core/design_system/foundations/grinta_elevation.dart
+++ lib/core/design_system/foundations/grinta_elevation.dart
@@ -14,27 +14,15 @@ abstract final class GrintaElevation {
   static const List<BoxShadow> none = [];
 
   static const List<BoxShadow> raisedShadow = [
-    BoxShadow(
-      color: Color(0x1A000000),
-      blurRadius: 8,
-      offset: Offset(0, 2),
-    ),
+    BoxShadow(color: Color(0x1A000000), blurRadius: 8, offset: Offset(0, 2)),
   ];
 
   static const List<BoxShadow> floatingShadow = [
-    BoxShadow(
-      color: Color(0x33000000),
-      blurRadius: 24,
-      offset: Offset(0, 10),
-    ),
+    BoxShadow(color: Color(0x33000000), blurRadius: 24, offset: Offset(0, 10)),
   ];
 
   static const List<BoxShadow> modalShadow = [
-    BoxShadow(
-      color: Color(0x52000000),
-      blurRadius: 40,
-      offset: Offset(0, 18),
-    ),
+    BoxShadow(color: Color(0x52000000), blurRadius: 40, offset: Offset(0, 18)),
   ];
 
   static const Border subtleBorder = Border.fromBorderSide(
