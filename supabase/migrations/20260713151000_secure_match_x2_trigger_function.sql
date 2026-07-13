f3f175c60377ba7dc019a7a14f5af28e5c3f45d9
samihchaa-wq <samihchaa@hotmail.com>
2026-07-13 13:41:04 +0200

    Sécuriser la fonction de contrôle des bonus x2

---
+++ supabase/migrations/20260713151000_secure_match_x2_trigger_function.sql
@@ -0,0 +1,3 @@
+revoke all on function public.enforce_match_prediction_x2() from public;
+revoke all on function public.enforce_match_prediction_x2() from anon;
+revoke all on function public.enforce_match_prediction_x2() from authenticated;
