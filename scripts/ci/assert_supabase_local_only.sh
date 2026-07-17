10e2090aea1d56591eb40b301676db79729fe79a
samihchaa-wq <samihchaa@hotmail.com>
2026-07-17 22:56:20 +0200

    ci: audit legacy match location shim

--- scripts/ci/assert_supabase_local_only.sh
+++ scripts/ci/assert_supabase_local_only.sh
@@ -15,6 +15,7 @@ if [ "$#" -eq 0 ]; then
     scripts/ci/supabase_legacy_claim_transition.sql
     scripts/ci/supabase_legacy_post_goals_views.sql
     scripts/ci/supabase_legacy_named_roster_transition.sql
+    scripts/ci/supabase_legacy_match_location.sql
   )
 else
   files=("$@")
