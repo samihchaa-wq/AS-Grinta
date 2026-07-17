d340b8eeba892b9a86d2fef31b12620ded69d003
samihchaa-wq <samihchaa@hotmail.com>
2026-07-17 22:43:12 +0200

    ci: audit post-goals compatibility shim

--- scripts/ci/assert_supabase_local_only.sh
+++ scripts/ci/assert_supabase_local_only.sh
@@ -13,6 +13,7 @@ if [ "$#" -eq 0 ]; then
     scripts/ci/supabase_legacy_coach_tables.sql
     scripts/ci/supabase_legacy_analytics_views.sql
     scripts/ci/supabase_legacy_claim_transition.sql
+    scripts/ci/supabase_legacy_post_goals_views.sql
   )
 else
   files=("$@")
