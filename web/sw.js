fd32ee2634648e39a612e1a82b65ea5b55854e35
samihchaa-wq <samihchaa@hotmail.com>
2026-07-21 18:06:57 +0200

    Actualiser le cache PWA du parcours sportif

--- web/sw.js
+++ web/sw.js
@@ -1,6 +1,6 @@
 // Service worker Ma Petite Grinta : cache réseau-d'abord (jamais de bundle périmé)
 // et réception des notifications push Web Push.
-const CACHE_NAME = 'as-grinta-v56';
+const CACHE_NAME = 'as-grinta-v57';
 
 self.addEventListener('install', () => {
   self.skipWaiting();
