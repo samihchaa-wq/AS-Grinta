f412f4c0f24b6a7fba9274d8e09454255374d966
samihchaa-wq <samihchaa@hotmail.com>
2026-07-25 23:49:28 +0200

    Tester le type de publication post-match

--- supabase/tests/database/postmatch_composition_permission.test.sql
+++ supabase/tests/database/postmatch_composition_permission.test.sql
@@ -1,7 +1,7 @@
 begin;
 
 set local search_path = public, extensions, pg_catalog;
-select plan(4);
+select plan(5);
 
 select ok(
   has_function_privilege(
@@ -37,6 +37,18 @@ select ok(
     'EXECUTE'
   ),
   'l’implémentation privée reste inaccessible aux anonymes'
+);
+
+select ok(
+  position(
+    '''postmatch''::text' in (
+      select pg_get_constraintdef(constraint_row.oid)
+      from pg_constraint constraint_row
+      where constraint_row.conrelid = 'public.match_composition_publications'::regclass
+        and constraint_row.conname = 'match_composition_publications_publication_kind_check'
+    )
+  ) > 0,
+  'la publication post-match est autorisée par la contrainte'
 );
 
 select * from finish();
