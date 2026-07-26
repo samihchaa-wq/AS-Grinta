2aa68c1c4199c543fa00662bfb60f6c3c41aba17
samihchaa-wq <samihchaa@hotmail.com>
2026-07-27 00:21:45 +0200

    fix: utiliser uniquement le service worker MPG

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,6 @@
+{{flutter_js}}
+{{flutter_build_config}}
+
+// Le service worker Flutter généré est volontairement désactivé : la PWA
+// utilise uniquement web/sw.js, qui gère aussi les notifications Web Push.
+_flutter.loader.load();
