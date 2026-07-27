26e389f686834e285e446517adc0a75d61a7e114
samihchaa-wq <samihchaa@hotmail.com>
2026-07-28 01:59:10 +0200

    flutter: sépare les composants du plan de match

--- lib/features/sports_management/presentation/admin_squad_plan_page.dart
+++ lib/features/sports_management/presentation/admin_squad_plan_page.dart
@@ -26,6 +26,7 @@ part 'admin_squad_plan_page_state.dart';
 part 'admin_squad_plan_page_effectif.dart';
 part 'admin_squad_plan_page_composition.dart';
 part 'admin_squad_plan_page_widgets.dart';
+part 'admin_squad_plan_page_composition_widgets.dart';
 
 enum _AdminStep { info, effectif, composition, prediction }
 
