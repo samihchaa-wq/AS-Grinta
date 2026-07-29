ac59ea4cf68c21dcd15f3d5ed8a9cda2f73c66b8
samihchaa-wq <samihchaa@hotmail.com>
2026-07-29 12:02:33 +0200

    feat(a11y): ajouter un cadre d’accessibilité global

---
+++ lib/core/widgets/grinta_accessibility_scope.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+/// Garantit un ordre de focus prévisible et un défilement utilisable au clavier,
+/// à la souris et au tactile sur toutes les plateformes.
+class GrintaAccessibilityScope extends StatelessWidget {
+  const GrintaAccessibilityScope({required this.child, super.key});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return FocusTraversalGroup(
+      policy: ReadingOrderTraversalPolicy(),
+      child: ScrollConfiguration(
+        behavior: const _GrintaScrollBehavior(),
+        child: Semantics(
+          container: true,
+          explicitChildNodes: false,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
+
+class _GrintaScrollBehavior extends MaterialScrollBehavior {
+  const _GrintaScrollBehavior();
+
+  @override
+  Set<PointerDeviceKind> get dragDevices => const {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.trackpad,
+        PointerDeviceKind.stylus,
+      };
+}
