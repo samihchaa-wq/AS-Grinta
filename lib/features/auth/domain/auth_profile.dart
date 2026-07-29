f163d7fc9bbce56edb6163e9aab650db7a4faf19
samihchaa-wq <samihchaa@hotmail.com>
2026-07-29 12:47:53 +0200

    Clarify profile role label documentation

--- lib/features/auth/domain/auth_profile.dart
+++ lib/features/auth/domain/auth_profile.dart
@@ -1,7 +1,7 @@
 enum AuthRole { pronostiqueur, admin }
 
 extension AuthRoleX on AuthRole {
-  /// Libellé d’affichage utilisé dans l’interface du profil.
+  /// Libellé lisible utilisé dans l’interface du profil.
   String get label {
     switch (this) {
       case AuthRole.admin:
