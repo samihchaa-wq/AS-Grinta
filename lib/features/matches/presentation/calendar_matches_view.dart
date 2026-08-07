a1adbec28ed8d1633aba818d0816df334d3bb345
samihchaa-wq <samihchaa@hotmail.com>
2026-08-08 01:05:26 +0200

    Calendrier : aligner l’ordre Par mois sur Défilé (#787)
    Affiche les matchs et événements du mode Par mois du plus ancien au plus futur, comme le flux Défilé, pour les saisons modernes et l’historique.

--- lib/features/matches/presentation/calendar_matches_view.dart
+++ lib/features/matches/presentation/calendar_matches_view.dart
@@ -533,7 +533,7 @@ class _ModernMonthView extends ConsumerWidget {
         if (_sameMonth(match.kickoffAt, month)) _ModernMonthEntry.match(match),
       for (final event in events)
         if (_sameMonth(event.startsAt, month)) _ModernMonthEntry.event(event),
-    ]..sort((a, b) => b.date.compareTo(a.date));
+    ]..sort((a, b) => a.date.compareTo(b.date));
 
     if (entries.isEmpty) {
       return RefreshIndicator(
@@ -653,7 +653,7 @@ class _HistoricalMonthView extends StatelessWidget {
           for (final event in events)
             if (_sameMonth(event.startsAt, month))
               _HistoricalMonthEntry.event(event),
-        ]..sort((a, b) => b.date.compareTo(a.date));
+        ]..sort((a, b) => a.date.compareTo(b.date));
 
         if (entries.isEmpty) {
           return RefreshIndicator(
