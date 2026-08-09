b16496dbb6e3ef84dd7beddbac47d99882a58619
samihchaa-wq <samihchaa@hotmail.com>
2026-08-09 15:56:13 +0200

    Nettoyer le cache d’historique

--- lib/features/matches/data/calendar_history_repository.dart
+++ lib/features/matches/data/calendar_history_repository.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:as_grinta/core/providers/supabase_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
