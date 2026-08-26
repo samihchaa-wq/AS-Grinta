c9b76e5669484cd20457ca0695592e5b8e6400ff
samihchaa-wq <samihchaa@hotmail.com>
2026-08-26 04:11:48 +0200

    Test historical absence completeness

---
+++ test/features/matches/data/historical_absent_names_test.dart
@@ -0,0 +1,27 @@
+import 'package:as_grinta/features/matches/data/historical_match_detail_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('absence inconnue reste null pour masquer le module Effectif', () {
+    final detail = historicalMatchDetailFromRow({
+      'present_names': ['Samih Chaa'],
+    });
+
+    expect(detail.presentNames, ['Samih']);
+    expect(detail.absentNames, isNull);
+  });
+
+  test('une liste absents explicite est conservée même vide', () {
+    final withoutAbsent = historicalMatchDetailFromRow({
+      'present_names': ['Samih Chaa'],
+      'absent_names': <String>[],
+    });
+    expect(withoutAbsent.absentNames, isEmpty);
+
+    final withAbsent = historicalMatchDetailFromRow({
+      'present_names': ['Samih Chaa'],
+      'absent_names': ['Florian Dupont'],
+    });
+    expect(withAbsent.absentNames, ['Florian']);
+  });
+}
